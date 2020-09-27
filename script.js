2e936dee1e9f802f5c7019ecb6a494c9fe810f74
ed-zsi <58634993+ed-zsi@users.noreply.github.com>
2020-09-27 09:48:54 +0200

    countdown

--- script.js
+++ script.js
@@ -6,4 +6,42 @@ window.onload = function () {
         nav.classList.toggle("active");
         btn.classList.toggle("active");
     });
+
+    const daysEl = document.getElementById("days");
+    const hoursEl = document.getElementById("hours");
+    const minsEl = document.getElementById("mins");
+    const secondsEl = document.getElementById("seconds");
+
+    const opening = "20 Jan 2050";
+
+    function countdown() {
+        const openingDate = new Date(opening);
+        const currentDate = new Date();
+
+        const totalSeconds = (openingDate - currentDate) / 1000;
+
+        const days = Math.floor(totalSeconds / 3600 / 24);
+        const hours = Math.floor(totalSeconds / 3600) % 24;
+        const mins = Math.floor(totalSeconds / 60) % 60;
+        const seconds = Math.floor(totalSeconds) % 60;
+
+        daysEl.innerHTML = days;
+        hoursEl.innerHTML = formatTime(hours);
+        minsEl.innerHTML = formatTime(mins);
+        secondsEl.innerHTML = formatTime(seconds);
+    }
+
+    function formatTime(time) {
+        return time < 10 ? `0${time}` : time;
+    }
+
+    // initial call
+    countdown();
+
+    setInterval(countdown, 1000);
+
 }
+
+
+//A vissza számláló kódot innen loptam: https://github.com/florinpop17/10-projects-10-hours
+//de tudom hogy működik mert már csináltam node.js ben
